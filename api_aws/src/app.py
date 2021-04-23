c953d2cb76d2e892c8b70de5d02b5b8191665cc0
hbrooks <seanhunterbrooks@gmail.com>
2021-04-23 14:52:59 -0400

    add api_aws;

--- api_aws/src/app.py
+++ api_aws/src/app.py
@@ -13,12 +13,7 @@ my_app = FastAPI()
 @my_app.get("/internal/ping")
 async def get_root(request: Request):
     try:
-        qldb_name = os.environ.get('QLDB_NAME', None)
-        qldb_driver = QldbDriver(ledger_name=qldb_name)
-        for table in qldb_driver.list_tables():
-            print(table)
         config = {} 
-        config['QLDB_NAME'] = qldb_name      
         return {"message": "pong", 'config': config}
     except:
         return {'message': 'bad'}
